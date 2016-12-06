0f922632b021752faa554b42215c66f4cfe34fc4
Azalee Bostroem <abostroem@gmail.com>
2016-12-05 20:29:45 -0800

    adding code to make a color wheel and plot multiple spectra

--- visualization.py
+++ visualization.py
@@ -1,5 +1,34 @@
 from astropy.visualization import ZScaleInterval
+from astropy.io import fits
+from matplotlib import pyplot
+import matplotlib
+
+
 def zscale(img):
     zscale = ZScaleInterval()
     vmin, vmax = zscale.get_limits(img)
     return vmin, vmax
+    
+def make_color_wheel(obj_list, cmap='rainbow'):
+    cmap = matplotlib.cm.get_cmap('gist_rainbow')
+    obj_color_list = cmap(np.linspace(0, 1, len(obj_list)))
+    return obj_color_list
+    
+    
+def plot_multiple_spectra(file_list, xcol='WAVELENGTH', ycol='FLUX', ax=None, colors = [], separation=0.5):
+    if ax is None:
+        fig = pyplot.figure(figsize=[10, 30])
+        ax = fig.add_subplot(1,1,1)
+    if len(colors)==0:
+        colors = make_color_wheel(file_list)
+    for indx, ifile in enumerate(file_list):
+        ofile = fits.open(ifile)
+        if indx ==0:
+            scale1 = np.sum(ofile[1].data[ycol])
+            offset = separation*np.max(ofile[1].data[ycol])
+        scale2 = np.sum(ofile[1].data[ycol])
+        l1=ax.plot(ofile[1].data[xcol], ofile[1].data[ycol]*scale1/scale2+offset*indx)
+        if len(colors)>0:
+            l1.set_color(colors[indx])
+    return ax
+    
