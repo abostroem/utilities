b413cbf2f9064441c943e0df6f124f2045a19b7e
Azalee Bostroem <abostroem@gmail.com>
2018-11-02 10:01:53 -0700

    making this a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='utilities_az',
+      version='0.1dev',
+      description='utilities functions and classes for astronomical analysis',
+      author='K. Azalee Bostroem',
+      author_email='abostroem@gmail.com',
+      packages=['utilities_az'],
+     ) 
