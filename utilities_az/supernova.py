add53e6b27358f11fee174400d4e65c5b6dbac55
Azalee Bostroem <abostroem@gmail.com>
2020-04-17 10:05:36 -0700

    Utilizing defaultdicts Adding ability to pull light curves from supernova Setting default values for when there is no extinction in the database Adding in telescope and project information to identify LCO and DLT40 data Fixing bug for if band is a string rather than a list Check for no jdexpl Add LCO lightcurve object Fix bug for how identifying when there is not photometry to scale to

--- utilities_az/supernova.py
+++ utilities_az/supernova.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 import sys
+from collections import defaultdict
+import warnings
 
 #Anaconda
 import numpy as np
@@ -14,7 +16,7 @@ import extinction
 import pymysql
 #Local
 from .define_filters import define_filters, get_cenwave
-from . import connect_to_sndavis
+from . import connect_to_sndavis, connect_to_supernova
 from . import spectroscopy as spec
 
 
@@ -34,16 +36,17 @@ class LightCurve(object):
         self.ebv_host_err = result['ebvierr']
         self.jdexpl = result['jdexpl']
         self.jdexpl_err = result['jdexplerr']
-        self.phase = {}
-        self.jd = {}
-        self.apparent_mag = {}
-        self.apparent_mag_err = {}
-        self.abs_mag = {}
-        self.abs_mag_err = {}
-        self.A_host = {}
-        self.A_err_host = {}
-        self.A_mw = {}
-        self.A_err_mw = {}
+        self.phase = defaultdict(list)
+        self.jd = defaultdict(list)
+        self.apparent_mag = defaultdict(list)
+        self.apparent_mag_err = defaultdict(list)
+        self.abs_mag = defaultdict(list)
+        self.abs_mag_err = defaultdict(list)
+        self.A_host = defaultdict(list)
+        self.A_err_host = defaultdict(list)
+        self.A_mw = defaultdict(list)
+        self.A_err_mw = defaultdict(list)
+        
     def add_photometry(filter, jd, phot, phot_err):
         self.jd[filter] = jd
         self.apparent_mag[filter] = phot
@@ -95,8 +98,13 @@ class LightCurve2(object):
             self.dist_mod_err = result['muerr']
             self.ebv_mw = result['ebvg']
             self.ebv_mw_err = result['ebvgerr']
-            self.ebv_host = result['ebvi'] 
-            self.ebv_host_err = result['ebvierr']
+            if result['ebvi'] is None:
+                self.ebv_host = 0
+                self.ebv_host_err = 0
+                print('WARNING: host extinciton is None, setting to 0')
+            else:
+                self.ebv_host = result['ebvi'] 
+                self.ebv_host_err = result['ebvierr']
             self.quality = result['quality']
             self.jdexpl = result['jdexpl']
             self.jdexpl_err = result['jdexplerr']
@@ -119,6 +127,10 @@ class LightCurve2(object):
             self.apparent_mag_err_limit = {}
             self.abs_mag_limit = {}
             self.abs_mag_err_limit = {}
+            self.telescope = {}
+            self.telescope_limit = {}
+            self.project = {}
+            self.project_limit = {}
         else:
             print('SN {} not found in database'.format(self.name))
             #TODO add an absolute magnitude error
@@ -140,7 +152,7 @@ class LightCurve2(object):
             results = self.cursor.fetchall()
             bands = [iband['BINARY(filter)'].decode('utf-8') for iband in results]
 
-        elif len(band)==1: #Single filter
+        elif len(band)==1 or isinstance(band, str): #Single filter
             bands = [band]
         else: #multiple filters
             bands = band
@@ -154,27 +166,66 @@ class LightCurve2(object):
                 A_err_host_band = 0
                 A_mw_band = 0
                 A_err_mw_band = 0
-            self.cursor.execute("SELECT jd, mag, magerr, source, datatype FROM photometry WHERE targetid={} AND filter=BINARY('{}')".format(self.id, ifilter))
+            #self.cursor.execute("SELECT jd, mag, magerr, datatype, configuration  FROM photometry JOIN photometrysource on photometry.source=photometrysource.source WHERE targetid={} AND filter=BINARY('{}')".format(self.id, ifilter))
+            self.cursor.execute("SELECT jd, mag, magerr, datatype, source FROM photometry WHERE targetid={} AND filter=BINARY('{}')".format(self.id, ifilter))
             results = self.cursor.fetchall()
             jd = []
             mag = []
             mag_err = []
+            telescope = []
+            project=[]
             jd_limit = []
             mag_limit = []
             mag_err_limit = []
+            telescope_limit = []
+            project_limit = []
             for irow in results:
-                if (irow['source'] < 0) or (irow['datatype'] < 0): #photometry represents a limit
+                if irow['magerr'] is None:
+                    irow['magerr'] = 0
+                if irow['datatype'] < 0: #photometry represents a limit
                     jd_limit.append(float(irow['jd']))
                     mag_limit.append(float(irow['mag']))
                     mag_err_limit.append(float(irow['magerr']))
+                    #telescope_limit.append(irow['configuration'])
+                    #telescope_limit.append(irow['source'])
+                    #if irow['configuration'].startswith('1m'):
+                    #    project_limit.append('LCO')
+                    #elif irow['configuration'] == 'Meckering' or irow['configuration'] == 'Prompt5':
+                    #    project_limit.append('DLT40')
+                    #else:
+                    #    project_limit.append(None)
+                    telescope_limit.append(irow['source'])
+                    if irow['source'] in [5209, 5109, 5009, 4809, 4909, 4709, 4609, 4509, 140, 141, 142, 165, 166, 167]:
+                        project_limit.append('LCO')
+                    elif irow['source'] in [5300, 5400]:
+                        project_limit.append('DLT40')
+                    else:
+                        project_limit.append(None)
                 else:
                     jd.append(float(irow['jd']))
                     mag.append(float(irow['mag']))
                     mag_err.append(float(irow['magerr']))
+                    # telescope.append(irow['configuration'])
+                    # if irow['configuration'].startswith('1m'):
+                    #     project.append('LCO')
+                    # elif irow['configuration'] == 'Meckering' or irow['configuration'] == 'Prompt5':
+                    #     project.append('DLT40')
+                    # else:
+                    #     project.append(None)
+                    telescope.append(irow['source'])
+                    if irow['source'] in [5209, 5109, 5009, 4809, 4909, 4709, 4609, 4509, 140, 141, 142, 165, 166, 167]:
+                        project.append('LCO')
+                    elif irow['source'] in [5300, 5400]:
+                        project.append('DLT40')
+                    else:
+                        project.append(None)
             self.jd[ifilter] = np.array(jd)
             self.apparent_mag[ifilter] = np.array(mag)
             self.apparent_mag_err[ifilter] = np.array(mag_err)
-            self.phase[ifilter] = self.jd[ifilter] - self.jdexpl
+            if self.jdexpl is not None:
+                self.phase[ifilter] = self.jd[ifilter] - self.jdexpl
+            else:
+                print('WARNING: No explosion epoch in database, phase is not calculated')
             self.A_host[ifilter] = A_host_band
             self.A_err_host[ifilter] = A_err_host_band
             self.A_mw[ifilter] = A_mw_band
@@ -182,7 +233,15 @@ class LightCurve2(object):
             self.jd_limit[ifilter] = np.array(jd_limit)
             self.apparent_mag_limit[ifilter] = np.array(mag_limit)
             self.apparent_mag_err_limit[ifilter] = np.array(mag_err_limit)
-            self.phase_limit[ifilter] = self.jd_limit[ifilter] - self.jdexpl
+            if self.jdexpl is not None:
+                self.phase_limit[ifilter] = self.jd_limit[ifilter] - self.jdexpl
+            else:
+                print('WARNING: No explosion epoch in database, phase is not calculated')
+            self.telescope[ifilter] = np.array(telescope)
+            self.telescope_limit[ifilter] = np.array(telescope_limit)
+            self.project[ifilter] = np.array(project)
+            self.project_limit[ifilter] = np.array(project_limit)
+            
 
             
     def get_abs_mag(self, band='all'):
@@ -244,6 +303,92 @@ class LightCurve2(object):
             self.slopes['{}_err'.format(slope_type)][band]   = None
             self.slopes['{}_range'.format(slope_type)][band] = [None, None]
 
+########################################
+
+class LightCurveLCO(object):
+    '''
+    Retrieve information from LCO supernova database
+    '''
+    def __init__(self, name, **kwargs):
+        if 'config_dir' not in kwargs.keys():
+            kwargs['config_dir'] = os.environ['LCOSNDIR']
+        if 'config_filename' not in kwargs.keys():
+            kwargs['config_filename'] = 'configure'
+        self.name=name
+        self.db, self.cursor = connect_to_supernova.get_cursor(**kwargs)
+        self.id = self.get_sn_id()
+        if self.id is not None:
+            self.cursor.execute('SELECT * FROM targets WHERE id = {}'.format(self.id))
+            result = self.cursor.fetchone()
+            self.ra = result['ra0']
+            self.dec = result['dec0']
+            self.type = result['classification'] #TODO - update this to look at the type table and make a human readable type
+            self.z = result['redshift']
+            #These are set by the get_photometry method
+            self.quality = {}
+            self.jd = {}
+            self.apparent_mag = {}
+            self.apparent_mag_err = {}
+            self.telescope = {}
+
+    def get_sn_id(self):
+        num_results = self.cursor.execute("SELECT DISTINCT targetid FROM targetnames WHERE name LIKE '%{}%'".format(self.name))
+        result = self.cursor.fetchall()
+        if num_results == 0:
+            return None
+        elif num_results>1:
+            warnings.warn('Multiple targetids match your query. Please enter the targetid you would like to select')
+            for iresult in result:
+                target = self.cursor.execute("SELECT name FROM targetnames WHERE targetid = {}".format(iresult['targetid']))
+                target_results = target.fetchall()
+                print('targetid: {}'.format(iresult['targetid']))
+                for itarget_result in target_results:
+                    print(itarget_result['name'])
+            targetid = input('Enter target id: ')
+        else:
+            targetid = result[0]['targetid']
+        return targetid
+ 
+    def get_photometry(self, band='all'):
+        '''
+        Get apparent magnitude of a specific band
+        '''
+        filter_dict =  define_filters()
+        if band == 'all':
+            self.cursor.execute("SELECT DISTINCT BINARY(filter) FROM photlco WHERE targetid={}".format(self.id))
+            results = self.cursor.fetchall()
+            bands = [iband['BINARY(filter)'].decode('utf-8') for iband in results]
+
+        elif len(band)==1 or isinstance(band, str): #Single filter
+            bands = [band]
+        else: #multiple filters
+            bands = band
+        for ifilter in bands:   
+            #self.cursor.execute("SELECT jd, mag, magerr, datatype, configuration  FROM photometry JOIN photometrysource on photometry.source=photometrysource.source WHERE targetid={} AND filter=BINARY('{}')".format(self.id, ifilter))
+            sql_str = "SELECT mjd, mag, dmag, telescope, quality FROM photlco WHERE"\
+                                + " wcs != 9999 AND (psf != 'X' AND psf != 9999) AND (zcat != 'X' OR abscat !='X') AND mag != 9999"\
+                                + " AND targetid={} AND filter=BINARY('{}')".format(self.id, ifilter)
+            self.cursor.execute(sql_str)
+            results = self.cursor.fetchall()
+            jd = []
+            mag = []
+            mag_err = []
+            quality = []
+            telescope = []
+            for irow in results:
+                if irow['dmag'] is None:
+                    irow['dmag'] = 0
+                jd.append(Time(float(irow['mjd']), format='mjd').jd)
+                mag.append(float(irow['mag']))
+                mag_err.append(float(irow['dmag']))
+                quality.append(int(irow['quality']))
+                telescope.append(irow['telescope'])
+            self.jd[ifilter] = np.array(jd)
+            self.apparent_mag[ifilter] = np.array(mag)
+            self.apparent_mag_err[ifilter] = np.array(mag_err)
+            self.quality[ifilter] = np.array(quality)
+            self.telescope[ifilter] = np.array(telescope)
+################
 
 def get_closest_photometry(date_obs, jd, phot_mag):
     '''
@@ -388,7 +533,7 @@ def scale_spectra_quba(snname, filename, filter_dir=None, date_kw='date-obs', ma
     band = ''
     mphot = []
     for ifilter in lightcurve.jd.keys():
-        if len(ifilter) == 1: #avoid swift filters
+        if ifilter not in ['us', 'vs', 'bs', 'uw1', 'uw2', 'um1', 'um2', 'UVW1', 'UVW2', 'UVM1', 'UVM2']: #avoid swift filters
             cenwave = get_cenwave(ifilter)
             if (cenwave > 3500):
                 mag, date_sep = get_interpolated_photometry(date_obs, lightcurve.jd[ifilter], lightcurve.apparent_mag[ifilter])
@@ -396,25 +541,28 @@ def scale_spectra_quba(snname, filename, filter_dir=None, date_kw='date-obs', ma
                     print('using {}={} interpolated to {} from {} and {}'.format(ifilter, mag, date_obs.iso,
                                                                                (date_sep[0]+date_obs).iso,
                                                                                (date_sep[1]+date_obs).iso))
-                    band=band+ifilter
+                    if ifilter.endswith('p'): #strip off p for LCO filters since qubascalespectra assumes filters are single character
+                        band = band+ifilter[0]
+                    else:
+                        band=band+ifilter
                     mphot.append(mag)
                 else:
                     if date_sep[0] is None:
                         print('No data for {} before {}'.format(ifilter, date_obs))
                     if date_sep[1] is None:
                         print('No data for {} after {}'.format(ifilter, date_obs))
-
-    if len(mphot) > 1:
+    if len(mphot) > 0:
         if verbose:
             print('bands: {}, photometry: {}'.format(band, mphot))
         qubascalespectra.scale_spectrum(filename, band, mphot, filter_dir)
+        shutil.move('log.txt', filename.split('.')[0]+'.log')
+        with open(filename.split('.')[0]+'.log', 'a') as ofile:
+            ofile.write(band+'\n')
+            mphot_str = [str(iphot) for iphot in mphot]
+            ofile.write(','.join(mphot_str)+'\n')
     else:
-        print('WARNING: no photometry within {} days of observation date ({})'.format(max_sep, date_obs))
-    shutil.move('log.txt', filename.split('.')[0]+'.log')
-    with open(filename.split('.')[0]+'.log', 'a') as ofile:
-        ofile.write(band+'\n')
-        mphot_str = [str(iphot) for iphot in mphot]
-        ofile.write(','.join(mphot_str)+'\n')
+        print('WARNING: no photometry within {} days of observation date ({}) ({})'.format(max_sep, date_obs, filename))
+    
         
             
     
