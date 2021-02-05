bbff4bb8ebef648c60ab9959dc3169a49b0b710c
Azalee Bostroem <abostroem@gmail.com>
2021-02-05 15:36:50 -0800

    adding flexibility to connection location

--- utilities_az/connect_to_supernova.py
+++ utilities_az/connect_to_supernova.py
@@ -10,19 +10,23 @@ def database_config(config_file):
     return config_dict
 
 def get_cursor(config_filename='configure', config_dir='/Users/bostroem/lco'):
-        config_dict = database_config(os.path.join(config_dir, config_filename))
-        if 'port' in config_dict.keys():
-            db = pymysql.connect(user=config_dict['mysqluser'], 
-                         host=config_dict['hostname'], 
-                         password=config_dict['mysqlpasswd'],
-                         database=config_dict['database'],
-                        port=int(config_dict['port']),
+    """
+    Connect to a database specified in <config_dir>/<config_filename>
+    e.g. local supernova: use defaults; dark supernova: config_filename='configure_dark'
+    """
+    config_dict = database_config(os.path.join(config_dir, config_filename))
+    if 'port' in config_dict.keys():
+        db = pymysql.connect(user=config_dict['mysqluser'], 
+                        host=config_dict['hostname'], 
+                        password=config_dict['mysqlpasswd'],
+                        database=config_dict['database'],
+                    port=int(config_dict['port']),
+                    cursorclass=pymysql.cursors.DictCursor)
+    else:
+        db = pymysql.connect(user=config_dict['mysqluser'], 
+                        host=config_dict['hostname'], 
+                        password=config_dict['mysqlpasswd'],
+                        database=config_dict['database'],
                         cursorclass=pymysql.cursors.DictCursor)
-        else:
-            db = pymysql.connect(user=config_dict['mysqluser'], 
-                         host=config_dict['hostname'], 
-                         password=config_dict['mysqlpasswd'],
-                         database=config_dict['database'],
-                         cursorclass=pymysql.cursors.DictCursor)
-        cursor = db.cursor()
-        return db, cursor
+    cursor = db.cursor()
+    return db, cursor
